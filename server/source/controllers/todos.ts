13043632dccbbb8b9e14d67952b1faaba9db1f85
Ironfist <ironfistse@gmail.com>
2022-09-05 16:55:58 +0200

    Move mock todos array to separated module.

--- server/source/controllers/todos.ts
+++ server/source/controllers/todos.ts
@@ -1,24 +1,8 @@
 import { Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { delay } from '../helpers';
+import { mockTodos } from '../mocks/data';
 
-const mockTodos = [
-  {
-    id: '1',
-    title: 'To buy meat',
-    isDone: true
-  },
-  {
-    id: '2',
-    title: 'To learn TypeScript',
-    isDone: false
-  },
-  {
-    id: '3',
-    title: 'To write todo app',
-    isDone: false
-  }
-];
 
 interface Todo {
   id: string
---
+++ server/source/mocks/data.ts
@@ -0,0 +1,17 @@
+export const mockTodos = [
+  {
+    id: '1',
+    title: 'To buy meat',
+    isDone: true
+  },
+  {
+    id: '2',
+    title: 'To learn TypeScript',
+    isDone: false
+  },
+  {
+    id: '3',
+    title: 'To write todo app',
+    isDone: false
+  }
+]
