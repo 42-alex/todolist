f5c3985f6afa31f100b434210d8cbd1fec7894d6
Ironfist <ironfistse@gmail.com>
2022-09-05 18:15:26 +0200

    Remove cors headers added by hand and use cors middleware instead.

--- server/source/server.ts
+++ server/source/server.ts
@@ -1,6 +1,7 @@
 import http from 'http';
 import express, { Express, Request, Response } from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import routes from './routes/todos'
 
 const router: Express = express();
@@ -11,22 +12,8 @@ router.use(morgan('dev'));
 router.use(express.urlencoded({ extended: false }));
 // takes care of json data
 router.use(express.json());
-
-// rules of our api
-router.use((req, res, next) => {
-  // set the CORS policy
-  res.header('Access-Control-Allow-Origin', '*');
-  // set the CORS headers
-  res.header('Access-Control-Allow-Headers', 'origin, X-Requested-With,Content-Type,Accept, Authorization');
-  // set the CORS method headers
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET PUT DELETE POST');
-    return res
-      .status(200)
-      .json({})
-  }
-  next();
-})
+// Enable All CORS Requests
+router.use(cors());
 
 
 // routes
