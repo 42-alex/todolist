4276afd138af98b48650ea9ef616aafb7191d1d5
Ironfist <ironfistse@gmail.com>
2022-09-11 13:40:14 +0200

    Add page navigation to the header.

--- front/src/pages/App/Header.tsx
+++ front/src/pages/App/Header.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import styles from './Header.module.scss';
+import { Link } from 'react-router-dom';
 
 const Header = (): JSX.Element => {
   return (
-    <div>This is header</div>
+    <header className={styles.header}>
+      <nav className={`${styles.headerNav} container`}>
+        <span className={styles.siteLogo}>
+          <a href="#" className={styles.siteLogoLink}>Logo</a>
+        </span>
+        <span className={styles.siteMenu}>
+          <Link to="/" className={styles.siteMenuLink}>Home</Link>
+          <Link to="/add" className={styles.siteMenuLink}>Add Todo</Link>
+          <Link to="/edit" className={styles.siteMenuLink}>Edit Todo</Link>
+        </span>
+      </nav>
+    </header>
   )
 }
 
