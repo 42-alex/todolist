c045bc7132fc2def47b2d744ca9abd62714f0b7d
Ironfist <ironfistse@gmail.com>
2022-09-15 19:49:27 +0200

    Add app reducer to keep common props

--- front/src/redux/store.ts
+++ front/src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import appReducer from './app-reducer';
 import todoReducer from './todo-reducer';
 import messagesReducer from './messages-reducer';
 
 const store = configureStore({
   reducer: {
+    app: appReducer,
     todos: todoReducer,
     messages: messagesReducer,
   }
