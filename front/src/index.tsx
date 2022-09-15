f18c2f558dffefa603b8d326269a72369ae6e5fb
Ironfist <ironfistse@gmail.com>
2022-09-15 15:20:50 +0200

    Add "react query" package.

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './pages/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import store from './redux/store';
 import './styles/main.scss';
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </QueryClientProvider>
 );
