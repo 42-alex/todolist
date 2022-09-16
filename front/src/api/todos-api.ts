7a8252c2840fd2726677957561054c6e5e0e6cfd
Ironfist <ironfistse@gmail.com>
2022-09-16 20:51:28 +0200

    Add usage of DTO.

--- front/src/api/todos-api.ts
+++ front/src/api/todos-api.ts
@@ -1,25 +1,52 @@
 import { Todo, TodosArr } from '../types';
 import { APIResponseType, axiosInstance } from './api';
+import { fromDTO, parseTodoDTO } from '../helpers';
 
 export const todosAPI = {
   getAllTodos() {
     return axiosInstance.get<APIResponseType<TodosArr>>('/todos')
-      .then(response =>  response.data.data)
+      .then(response => {
+        const todosArr = response.data.data.map(todo => {
+          return fromDTO(parseTodoDTO(todo))
+        })
+
+        return todosArr;
+      })
   },
   getTodo(id: number) {
     return axiosInstance.get<Todo>(`/todos/${id}`)
-      .then(response => response.data)
+      .then(response => {
+        const todoDTO = parseTodoDTO(response.data);
+        const todo = fromDTO(todoDTO);
+
+        return todo;
+      })
   },
   updateTodo(id: number, newTitle: string) {
     return axiosInstance.put<Todo>(`/todos/${id}`, { title: newTitle })
-      .then(response => response.data)
+      .then(response => {
+        const todoDTO = parseTodoDTO(response.data);
+        const todo = fromDTO(todoDTO);
+
+        return todo;
+      })
   },
   deleteTodo(id: number) {
     return axiosInstance.delete<Todo>(`/todos/${id}`)
-      .then(response => response.data)
+      .then(response => {
+        const todoDTO = parseTodoDTO(response.data);
+        const todo = fromDTO(todoDTO);
+
+        return todo;
+      })
   },
   addTodo(title: string) {
     return axiosInstance.post<Todo>('/todos', { title })
-      .then(response => response.data)
+      .then(response => {
+        const todoDTO = parseTodoDTO(response.data);
+        const todo = fromDTO(todoDTO);
+
+        return todo;
+      })
   },
 }
