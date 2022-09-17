b4be4e64c05a0e89df735b8c1838cf1755e922f0
Ironfist <ironfistse@gmail.com>
2022-09-17 21:15:32 +0200

    Review fix: remove attempts to guess the server props (we actually know them)

--- front/src/helpers/index.ts
+++ front/src/helpers/index.ts
@@ -6,42 +6,21 @@ const parseTodoDTO = (data: {[propName: string]: any}) => {
   // check id
   if('id' in data) {
     todoDTO.id = data.id;
-  }
-  else if ('todoId' in data) {
-    todoDTO.id = data.todoId;
-  }
-  else if ('itemId' in data) {
-    todoDTO.id = data.itemId;
-  }
-  else {
+  } else {
     throw new Error('There is no todo id. I can\'t work without it')
   }
 
   // check title
-  if('todoTitle' in data) {
-    todoDTO.title = data.todoTitle;
-  }
-  else if ('text' in data) {
-    todoDTO.title = data.text;
-  }
-  else if ('title' in data) {
+  if ('title' in data) {
     todoDTO.title = data.title;
-  }
-  else {
+  } else {
     throw new Error(`There is no known key for title in todo with id: ${todoDTO.id}`)
   }
 
   // check isDone
-  if('isCompleted' in data) {
-    todoDTO.isDone = data.isCompleted;
-  }
-  else if ('isSucceed' in data) {
-    todoDTO.isDone = data.isSucceed;
-  }
-  else if ('isDone' in data) {
+  if ('isDone' in data) {
     todoDTO.isDone = data.isDone;
-  }
-  else { // set false by default
+  } else { // set false by default
     console.log(`A prop "isDone" for the todo with id: ${todoDTO.id} was set to false as there was not find appropriate key from response`);
     todoDTO.isDone = false;
   }
