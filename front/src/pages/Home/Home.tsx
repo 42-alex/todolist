09aa44a16bf178261f42e03e568b05a066fc7101
Ironfist <ironfistse@gmail.com>
2022-09-19 19:51:21 +0200

    Review fix: do not use redux for keep todos array there (it is the react query's responsibility)

--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -3,31 +3,27 @@ import { useQuery } from '@tanstack/react-query';
 import { todosAPI } from '../../api/todos-api';
 import { TodosArr } from '../../types';
 import { useDispatch, useSelector } from 'react-redux';
-import { RootStateType } from '../../redux/store';
-import { setTodos } from '../../redux/todo-reducer';
 import { AxiosError } from 'axios';
 import { addMessage } from '../../redux/messages-reducer';
 import styles from './Home.module.scss';
 import Loader from '../../components/Loader';
 
 type useQueryType = {
+  data: TodosArr | undefined,
   error: AxiosError | null
   [propName: string]: any
 }
 
 const Home = (): JSX.Element => {
   const dispatch = useDispatch();
-  const { todos } = useSelector((state: RootStateType) => state.todos)
 
   const {
+    data: todos = [],
     isFetching,
   }: useQueryType = useQuery(
     ['todos'],
     todosAPI.getAllTodos,
     {
-      onSuccess: (todosArr: TodosArr ) => {
-        dispatch(setTodos(todosArr));
-      },
       onError: (error) => {
         dispatch(addMessage({
           text: error?.message || '',
@@ -42,7 +38,7 @@ const Home = (): JSX.Element => {
 
   return (
     <div className="container">
-      { todos.length > 0 &&
+      { todos?.length > 0 &&
         <ul className={styles.todosList}>
           {todos.map(todo => (
             <li key={todo.id}
--- front/src/redux/store.ts
+++ front/src/redux/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import appReducer from './app-reducer';
-import todoReducer from './todo-reducer';
 import messagesReducer from './messages-reducer';
 
 const store = configureStore({
   reducer: {
     app: appReducer,
-    todos: todoReducer,
     messages: messagesReducer,
   }
 })
