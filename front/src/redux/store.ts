c1501a5fff666b37c275d27e9c9b43b3a034acc0
Ironfist <ironfistse@gmail.com>
2022-09-19 19:52:56 +0200

    Review fix: remove app reduces

--- front/src/redux/store.ts
+++ front/src/redux/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import appReducer from './app-reducer';
 import messagesReducer from './messages-reducer';
 
 const store = configureStore({
   reducer: {
-    app: appReducer,
     messages: messagesReducer,
   }
 })
