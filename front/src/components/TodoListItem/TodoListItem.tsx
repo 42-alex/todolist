728214ffb6487e3829bc45088491c032f923ccda
Ironfist <ironfistse@gmail.com>
2022-10-07 12:01:36 +0200

    Move TodoListItem component to a separated folder.

---
+++ front/src/components/TodoListItem/TodoListItem.tsx
@@ -0,0 +1,55 @@
+import styles from '../pages/Home/Home.module.scss';
+import CheckedIcon from './../Icons/CheckedIcon';
+import UncheckedIcon from './../Icons/UncheckedIcon';
+import { importanceTheme } from '../../constants';
+import PencilIcon from './../Icons/PencilIcon';
+import BallotXIcon from './../Icons/BallotXIcon';
+import React from 'react';
+import { Todo } from '../../types';
+
+interface ITodoListItem {
+  todo: Todo
+  onClick: (id: string, isDone: boolean) => void
+  onEdit: (ev: React.MouseEvent<HTMLButtonElement>, todoId: string) => void
+  onDelete: (ev: React.MouseEvent<HTMLButtonElement>, todoId: string) => void
+}
+
+const TodoListItem = ({ todo, onClick, onEdit, onDelete }: ITodoListItem) => {
+  return (
+    <div className={styles.todoWrapper} key={todo.id} onClick={() => onClick(todo.id, !todo.isDone)}>
+      <li className={styles.todoItem}>
+        <div className={styles.todoItemContent}>
+                  <span className={styles.checkIcon}>
+                    {todo.isDone ? <CheckedIcon /> : <UncheckedIcon />}
+                  </span>
+          <div className="todoTitle">
+            {todo.title}
+          </div>
+          { todo.importance !== 'ordinary' &&
+          <div className={`${styles.ribbon} ${styles.ribbonTopRight}`}>
+            <span style={{backgroundColor: importanceTheme[todo.importance]}}>{todo.importance}</span>
+          </div>
+          }
+        </div>
+        <div className={styles.todoItemActions}>
+          <button
+            className={styles.editButton}
+            title="Edit todo"
+            onClick={(ev) => onEdit(ev, todo.id)}
+          >
+            <PencilIcon />
+          </button>
+          <button
+            className={styles.deleteButton}
+            title="Delete todo"
+            onClick={(ev) => onDelete(ev, todo.id)}
+          >
+            <BallotXIcon />
+          </button>
+        </div>
+      </li>
+    </div>
+  );
+};
+
+export default TodoListItem;
--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import styles from './Home.module.scss';
 import Loader from '../../components/Loader';
-import { importanceTheme } from '../../constants';
 import useUpdateTodo from '../../hooks/useUpdateTodo';
 import useFetchTodos from '../../hooks/useFetchTodos';
 import useDeleteTodo from '../../hooks/useDeleteTodo';
 import { Link, useNavigate } from 'react-router-dom';
-import CheckedIcon from '../../components/Icons/CheckedIcon';
-import UncheckedIcon from '../../components/Icons/UncheckedIcon';
-import PencilIcon from '../../components/Icons/PencilIcon';
-import BallotXIcon from '../../components/Icons/BallotXIcon';
+import TodoListItem from '../../components/TodoListItem';
 
 
 const Home = () => {
@@ -42,39 +38,12 @@ const Home = () => {
       { todos.length > 0 &&
         <ul className={styles.todosList}>
           {todos.map(todo => (
-            <div className={styles.todoWrapper} key={todo.id} onClick={() => handleTodoClick(todo.id, !todo.isDone)}>
-              <li className={styles.todoItem}>
-                <div className={styles.todoItemContent}>
-                  <span className={styles.checkIcon}>
-                    {todo.isDone ? <CheckedIcon /> : <UncheckedIcon />}
-                  </span>
-                  <div className="todoTitle">
-                    {todo.title}
-                  </div>
-                  { todo.importance !== 'ordinary' &&
-                    <div className={`${styles.ribbon} ${styles.ribbonTopRight}`}>
-                      <span style={{backgroundColor: importanceTheme[todo.importance]}}>{todo.importance}</span>
-                    </div>
-                  }
-                </div>
-                <div className={styles.todoItemActions}>
-                  <button
-                    className={styles.editButton}
-                    title="Edit todo"
-                    onClick={(ev) => handleEditButtonClick(ev, todo.id)}
-                  >
-                    <PencilIcon />
-                  </button>
-                  <button
-                    className={styles.deleteButton}
-                    title="Delete todo"
-                    onClick={(ev) => handleDeleteButtonClick(ev, todo.id)}
-                  >
-                    <BallotXIcon />
-                  </button>
-                </div>
-              </li>
-            </div>
+            <TodoListItem
+              todo={todo}
+              onClick={handleTodoClick}
+              onEdit={handleEditButtonClick}
+              onDelete={handleDeleteButtonClick}
+            />
           ))}
         </ul>
       }
